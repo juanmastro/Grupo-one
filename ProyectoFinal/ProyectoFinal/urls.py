b19494a67368fc4eafa733f161234a17e91a7857
Lucas Ferrero <lucas.terrenal@gmail.com>
2021-12-13 22:29:15 -0300

    Incluyo python pruebo runserver

--- ProyectoFinal/ProyectoFinal/urls.py
+++ ProyectoFinal/ProyectoFinal/urls.py
@@ -1,4 +1,4 @@
-"""Proyecto1 URL Configuration
+"""ProyectoFinal URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,19 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ProyectoFinal.views import saludo, segundaVista, dia, probandoTemplate
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path ("saludo/", saludo),
-
-    path ("loke/", segundaVista),
-
-    path ("dia/", dia),
-
-    path ("templates/",probandoTemplate),
-
-
 ]
