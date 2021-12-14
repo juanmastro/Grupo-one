61781228cb56d7221e69fb152c65bd43f01b5b0a
Lucas Ferrero <lucas.terrenal@gmail.com>
2021-12-13 22:22:42 -0300

    Incluyo python pruebo runserver v1

--- ProyectoFinal/ProyectoFinal/urls.py
+++ ProyectoFinal/ProyectoFinal/urls.py
@@ -1,4 +1,4 @@
-"""ProyectoFinal URL Configuration
+"""Proyecto1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,7 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ProyectoFinal.views import saludo, segundaVista, dia, probandoTemplate
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path ("saludo/", saludo),
+
+    path ("loke/", segundaVista),
+
+    path ("dia/", dia),
+
+    path ("templates/",probandoTemplate),
+
+
 ]
--- ProyectoFinal/ProyectoFinal/wsgi.py
+++ ProyectoFinal/ProyectoFinal/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ProyectoFinal project.
+WSGI config for Proyecto1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProyectoFinal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Proyecto1.settings')
 
 application = get_wsgi_application()
