007c6ee4342f4af5dba0d03854bdefc9acf6160d
Lucas Ferrero <lucas.terrenal@gmail.com>
2021-12-14 15:43:50 -0300

    Se genera template con hora actualizada cargador y Appcoder DB

---
+++ ProyectoFinal/AppCoder/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class persona (models.Model):
+    nombre =  models.CharField (max_length=40)
+    edad = models.IntegerField ()
+
+class pelicula (models.Model):
+    nombre =  models.CharField (max_length=40)
+    genero = models.CharField (max_length=40)
--- ProyectoFinal/ProyectoFinal/settings.py
+++ ProyectoFinal/ProyectoFinal/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'AppCoder',
+]   
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
