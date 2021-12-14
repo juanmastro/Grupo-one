4f05f7b84c520c6322df1c1327c6b5b71d7cd842
Lucas Ferrero <lucas.terrenal@gmail.com>
2021-12-14 11:55:33 -0300

    Se genera template con hora actualizada

--- ProyectoFinal/ProyectoFinal/views.py
+++ ProyectoFinal/ProyectoFinal/views.py
@@ -6,12 +6,16 @@ def saludo (request):
     return HttpResponse ("Bienvenido a la WEB Pelis-Pro-Play")
 
 def templateUno (seft):
+
+    fecha = datetime.now ()
+    diccionario = {"fechaActualizada": fecha}
+
     mihtml = open ("C:\\Users\\u900768\\Desktop\\Proyecto\\Grupo-one\\ProyectoFinal\\ProyectoFinal\\plantillas\\template1.html")
     pantilla = Template (mihtml.read())
     
     mihtml.close()
 
-    miContexto = Context ()
+    miContexto = Context (diccionario)
     documento = pantilla.render (miContexto)
 
     return HttpResponse (documento)
