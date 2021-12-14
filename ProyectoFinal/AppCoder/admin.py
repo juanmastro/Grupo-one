421335e5df4c030b6e5c8b808e0dcf7df4503fc0
Lucas Ferrero <lucas.terrenal@gmail.com>
2021-12-14 20:32:07 -0300

    Agrego clases a admin.py

--- ProyectoFinal/AppCoder/admin.py
+++ ProyectoFinal/AppCoder/admin.py
@@ -1,3 +1,11 @@
+
 from django.contrib import admin
 
 # Register your models here.
+from.models import *
+
+
+
+admin.site.register(persona)
+admin.site.register(pelicula)
+admin.site.register(series)
