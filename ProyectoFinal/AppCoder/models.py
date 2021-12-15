f20252fd22f7230c0ee4c52f5c3b7e1139c3ebe4
juanmastro <94941222+juanmastro@users.noreply.github.com>
2021-12-14 22:49:06 -0300

    Update models.py
    Agrego Apellido a Persona

--- ProyectoFinal/AppCoder/models.py
+++ ProyectoFinal/AppCoder/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class persona (models.Model):
     nombre =  models.CharField (max_length=40)
+    apellido =  models.CharField (max_length=40)
     edad = models.IntegerField ()
 
 class pelicula (models.Model):
